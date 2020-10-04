fdfc4522c40d2f72a308d196889ba40fc8ebb36b
thorio <thorio.git@gmail.com>
2020-10-04 17:50:35 +0200

    Add version verb

---
+++ src/blent/Constants.cs
@@ -0,0 +1,10 @@
+namespace Blent
+{
+	public static class Constants
+	{
+		public const string ProjectUrl = "https://github.com/thorio/blent#readme";
+		public const string RepositoryUrl = "https://github.com/thorio/blent";
+		public const string IssuesUrl = "https://github.com/thorio/blent/issues";
+		public const string License = "GPL-3.0";
+	}
+}
