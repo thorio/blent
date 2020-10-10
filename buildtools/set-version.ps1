8e0376c26d51a3af13dceecd84b2b7237a7a38f0
thorio <thorio.git@gmail.com>
2020-10-10 12:22:22 +0200

    Update set-version.ps1

--- buildtools/set-version.ps1
+++ buildtools/set-version.ps1
@@ -1,12 +1,19 @@
 # extract package version
 [xml]$csproj = Get-Content src/Blent/Blent.csproj
-$version += $csproj.Project.PropertyGroup.Version
-$version = "$version".Trim()
+$version = "${csproj.Project.PropertyGroup.Version}".Trim()
+$sha = "${env:APPVEYOR_REPO_COMMIT}".Substring(0, 8)
 
 if ($env:APPVEYOR_REPO_BRANCH -ne "master") {
-	$branch = $env:APPVEYOR_REPO_BRANCH -replace '[/]+','-'
-	$version += "-indev+${branch}"
+	$branch = $env:APPVEYOR_REPO_BRANCH -replace '[^\d\w]+','-'
+	$version += "-${branch}+${sha}"
+} elseif ($env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT -eq "") {
+	$pr_branch = $env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH -replace '[^\d\w]+','-'
+	$version += "-pr-${pr_branch}+${sha}"
 }
 
-Update-AppveyorBuild -Version "$version"
+try {
+	Update-AppveyorBuild -Version "$version"
+} catch {
+	Exit-AppveyorBuild # can happen when the version is not unique.
+}
 echo "set version: $env:APPVEYOR_BUILD_VERSION"
