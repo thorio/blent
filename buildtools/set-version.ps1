b57e20937a81bee74706c45b789ce2c7998ab774
thorio <thorio.git@gmail.com>
2020-10-10 13:20:33 +0200

    Update versioning

--- buildtools/set-version.ps1
+++ buildtools/set-version.ps1
@@ -1,12 +1,25 @@
 # extract package version
 [xml]$csproj = Get-Content src/Blent/Blent.csproj
-$version += $csproj.Project.PropertyGroup.Version
+$version = $csproj.Project.PropertyGroup.Version
 $version = "$version".Trim()
 
-if ($env:APPVEYOR_REPO_BRANCH -ne "master") {
-	$branch = $env:APPVEYOR_REPO_BRANCH -replace '[/]+','-'
-	$version += "-indev+${branch}"
+$sha = "${env:APPVEYOR_REPO_COMMIT}".Substring(0, 8)
+
+if ($env:APPVEYOR_REPO_BRANCH -eq "master") {
+	# release build (1.0.0)
+} elseif ($env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT -ne $null) {
+	# pr build (1.0.0-pr-feature-version-verb+8e0376c2)
+	$pr_branch = $env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH -replace '[^\d\w]+','-'
+	$version += "-pr-${pr_branch}+${sha}"
+} else {
+	# branch build (1.0.0-develop+8e0376c2)
+	$branch = $env:APPVEYOR_REPO_BRANCH -replace '[^\d\w]+','-'
+	$version += "-${branch}+${sha}"
 }
 
-Update-AppveyorBuild -Version "$version"
+try {
+	Update-AppveyorBuild -Version "$version"
+} catch {
+	Exit-AppveyorBuild # can happen when the version is not unique.
+}
 echo "set version: $env:APPVEYOR_BUILD_VERSION"
