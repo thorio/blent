b7546e27d7fa3fa11ac3364aa11ae2dec8fde126
thorio <thorio.git@gmail.com>
2020-10-10 12:23:22 +0200

    Update set-version.ps1

--- buildtools/set-version.ps1
+++ buildtools/set-version.ps1
@@ -1,6 +1,7 @@
 # extract package version
 [xml]$csproj = Get-Content src/Blent/Blent.csproj
-$version = "${csproj.Project.PropertyGroup.Version}".Trim()
+$version = $csproj.Project.PropertyGroup.Version
+$version = "$version".Trim()
 $sha = "${env:APPVEYOR_REPO_COMMIT}".Substring(0, 8)
 
 if ($env:APPVEYOR_REPO_BRANCH -ne "master") {
