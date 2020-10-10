f45b5a7ef6842bc45120159fdae1bcfe318d9fdf
thorio <thorio.git@gmail.com>
2020-10-10 13:09:47 +0200

    Update set-version.ps1

--- buildtools/set-version.ps1
+++ buildtools/set-version.ps1
@@ -2,14 +2,17 @@
 [xml]$csproj = Get-Content src/Blent/Blent.csproj
 $version = $csproj.Project.PropertyGroup.Version
 $version = "$version".Trim()
+
 $sha = "${env:APPVEYOR_REPO_COMMIT}".Substring(0, 8)
 
 if ($env:APPVEYOR_REPO_BRANCH -eq "master") {
-	# append nothing
+	# release build (1.0.0)
 } elseif ($env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT -ne $null) {
+	# pr build (1.0.0-pr-feature-version-verb+8e0376c2)
 	$pr_branch = $env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH -replace '[^\d\w]+','-'
 	$version += "-pr-${pr_branch}+${sha}"
-} elseif ($env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT -ne "") {
+} else {
+	# branch build (1.0.0-develop+8e0376c2)
 	$branch = $env:APPVEYOR_REPO_BRANCH -replace '[^\d\w]+','-'
 	$version += "-${branch}+${sha}"
 }
