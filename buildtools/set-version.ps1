b8e5ada77f0e6a6ce239aa02ed49feb3a2b6bb31
thorio <thorio.git@gmail.com>
2020-10-10 12:34:18 +0200

    Update set-version.ps1

--- buildtools/set-version.ps1
+++ buildtools/set-version.ps1
@@ -4,14 +4,15 @@ $version = $csproj.Project.PropertyGroup.Version
 $version = "$version".Trim()
 $sha = "${env:APPVEYOR_REPO_COMMIT}".Substring(0, 8)
 
-if (($env:APPVEYOR_REPO_BRANCH -ne "master") -and ($env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT -eq "")) {
-	$branch = $env:APPVEYOR_REPO_BRANCH -replace '[^\d\w]+','-'
-	$version += "-${branch}+${sha}"
-} elseif ($env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT -ne "") {
+if ($env:APPVEYOR_REPO_BRANCH -eq "master") {
+	# append nothing
+} elseif (Test-Path '$env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT')
 	$pr_branch = $env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH -replace '[^\d\w]+','-'
 	$version += "-pr-${pr_branch}+${sha}"
+} elseif ($env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT -ne "") {
+	$branch = $env:APPVEYOR_REPO_BRANCH -replace '[^\d\w]+','-'
+	$version += "-${branch}+${sha}"
 }
-echo "|${env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT}|"
 
 try {
 	Update-AppveyorBuild -Version "$version"
